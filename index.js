4c5d4b199db6cb6f071feee539325e29de59dbd2
Cloud-Xero <hiropon201511@gmail.com>
2022-07-23 15:46:29 +0900

    fix: port number

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-import express from 'express';
-import useRoutes from './routes/users.js'; // userRoutesをインポート
+import express from "express";
+import useRoutes from "./routes/users.js"; // userRoutesをインポート
 
 const app = express(); // express のインスタンス化
 const PORT = 3000;
 
-app.use('/users', useRoutes); //ミドルウェア定義
-
 // パスにリクエストが入ると、後のコールバック関数が呼び出される
-app.get('/', (request, response) => {
-  response.send('Welcome');
+app.get("/", (request, response) => {
+  response.send("Welcome");
 });
 
-// listen() メソッドを実行して、5000番ポートで待ち受け
+// listen() メソッドを実行して、3000番ポートで待ち受け
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
